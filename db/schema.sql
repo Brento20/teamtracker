b43e35245b95660170e4137608335357790c1ce4
Brenton Weaver <brenton.weaver@gmail.com>
2022-02-23 23:01:57 +1100

    created a promt funtion that is going to allow me to switch between desired funtions for my database

--- db/schema.sql
+++ db/schema.sql
@@ -3,9 +3,9 @@ CREATE DATABASE mycompany_db;
 
 USE mycompany_db;
 
-CREATE TABLE employee_names (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
+-- CREATE TABLE employee_names (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     first_name VARCHAR(30) NOT NULL,
+--     last_name VARCHAR(30) NOT NULL,
     
-);
+-- );
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const inquirer = require("inquirer");
 const mysql = require('mysql2');
 require('dotenv').config();
 
@@ -18,8 +19,47 @@ const db = mysql.createConnection(
 );
 
 
+function startPrompt() {
+    console.log("Welcome to my company");
+    inquirer.prompt([
+    {
+    type: "list",
+    message: "What would you like to do?",
+    name: "choice",
+    choices: [
+            "View All Employees", 
+            "Group Employees By Roles",
+            "Group Employees By Department", 
+            ]
+    }
+])
+        .then( answer => {
+
+        switch (answer.choice) {
+            case 'View All Employees':
+                console.log('option1');
+                // write a function to show all employees in the database then call the start prompt function again
+                break;
+            case 'Group Employees By Roles':
+                console.log('option2');
+                // write a function to show all employees grouped by role in the database then call the start prompt function again
+                break;
+            case 'Group Employees By Department':
+                console.log('option3');
+                // write a function to show all employees grouped by department in the database then call the start prompt function again
+                break;
+            default:
+                console.log(`Unable to select options for ${choices}`);
+        }
+    })
+};
+
+
+
 //Things go here
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+startPrompt();
